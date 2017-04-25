7f5155769232cd016577e99b7b197f804cf5f8f8
IhorShmidt <ihor.shmidt@techmagic.co>
2017-04-25 18:48:39 +0300

    base chat implementation

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
-var socket = io();
+const socket = io();
 
-socket.on('connect', () => console.log('connected via socket'));
+socket.on('connect', (socket) => {
+    console.log('connected via socket');
+});
+
+socket.on('message', (message) => {
+    console.log(message.text);
+});
+
+const $form = jQuery('#message-form');
+
+$form.on('submit', (event) => {
+    event.preventDefault();
+    const message = $form.find('input[name=message]');
+
+    socket.emit('message', {text: message.val()});
+    message.val('');
+});
--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-io.on('connection', () => console.log('connected via socket.io'));
+const dictionary = {
+    connection: 'connection',
+    message: 'message'
+};
+
+io.on(dictionary.connection, (socket) => {
+    console.log('connected via socket.io');
+
+    socket.on(dictionary.message, (message) => {
+        socket.broadcast.emit(dictionary.message, message);
+    });
+
+});
 
 app.use(express.static(`${__dirname}/public`));
 
